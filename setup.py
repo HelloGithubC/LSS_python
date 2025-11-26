c7a5dbab4d7848272e583028805ca6376a345c68
HelloGithubC <xiaoliang5@mail2.sysu.edu.cn>
2025-11-26 10:25:30 +0800

    Add support files for publishing

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from setuptools.command.build_ext import build_ext
 import subprocess
-import sys
+import sys, glob
 import os
 
 class CustomBuildExt(build_ext):
@@ -25,49 +25,56 @@ class CustomBuildExt(build_ext):
             print(result.stderr)
             sys.exit(result.returncode)
         else:
+            so_files = glob.glob("src/LSS_python/CPP/lib/*.so")
+            if not so_files:
+                raise RuntimeError("No .so files found after make! Check Makefile output path.")
             print("C++ extensions built successfully")
             if result.stdout:
                 print(result.stdout)
 
 # 由于你通过Makefile直接编译.so文件，我们不需要定义传统的Extension
 # 但需要确保.so文件被包含在包中
+# setup(
+#     name="LSS_python",
+#     version="0.5.0",
+    
+#     # 包发现配置
+#     package_dir={"": "src"},
+#     packages=find_packages(where="src"),
+    
+#     # 包含共享库文件
+#     package_data={
+#         'LSS_python.CPP': ['lib/*.so'],
+#     },
+    
+#     # 确保包中包含.so文件
+#     include_package_data=True,
+    
+#     # 使用自定义构建命令
+#     cmdclass={
+#         'build_ext': CustomBuildExt,
+#     },
+    
+#     # 安装要求
+#     install_requires=[
+#         "numpy",
+#         "numba==0.61.0",
+#         "joblib",
+#         "scipy",
+#         "emcee",
+#         "h5py",
+#         "multiprocess",
+#         "Corrfunc",
+#     ],
+    
+#     # 其他元数据
+#     author="Liang Xiao",
+#     author_email="xiaoliangms@outlook.com",
+#     description="A package to deal with some problems about Large Scale Structure. Full support by python(numba). cuda(cupy) is optinonal",
+#     python_requires=">=3.6",
+#     zip_safe=False,  # 由于包含共享库，设置为False
+# )
+
 setup(
-    name="LSS_python",
-    version="0.5.0",
-    
-    # 包发现配置
-    package_dir={"": "src"},
-    packages=find_packages(where="src"),
-    
-    # 包含共享库文件
-    package_data={
-        'LSS_python.CPP': ['src/LSS_python/CPP/lib/*.so'],
-    },
-    
-    # 确保包中包含.so文件
-    include_package_data=True,
-    
-    # 使用自定义构建命令
-    cmdclass={
-        'build_ext': CustomBuildExt,
-    },
-    
-    # 安装要求
-    install_requires=[
-        "numpy",
-        "numba==0.61.0",
-        "joblib",
-        "scipy",
-        "emcee",
-        "h5py",
-        "multiprocess",
-        "Corrfunc",
-    ],
-    
-    # 其他元数据
-    author="Liang Xiao",
-    author_email="xiaoliangms@outlook.com",
-    description="A package to deal with some problems about Large Scale Structure. Full support by python(numba). cuda(cupy) is optinonal",
-    python_requires=">=3.6",
-    zip_safe=False,  # 由于包含共享库，设置为False
+    cmdclass={"build_ext": CustomBuildExt}
 )
